d4ccdf6c9297d34073b35a34f549f44f19f8c1fd
sicyrillewilson <sicyrillewilson@gmail.com>
2025-04-01 17:56:14 +0000

    cyrille18

--- LocUL/app/src/main/java/tg/univlome/epl/ui/batiment/AllBatimentFragment.kt
+++ LocUL/app/src/main/java/tg/univlome/epl/ui/batiment/AllBatimentFragment.kt
@@ -1,13 +1,23 @@
+@file:Suppress("DEPRECATION")
+
 package tg.univlome.epl.ui.batiment
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import org.osmdroid.util.GeoPoint
 import tg.univlome.epl.MainActivity
 import tg.univlome.epl.R
 import tg.univlome.epl.adapter.BatimentFragmentAdapter
@@ -17,11 +27,14 @@ import tg.univlome.epl.ui.SearchBarFragment
 
 class AllBatimentFragment : Fragment(), SearchBarFragment.SearchListener {
 
-    private lateinit var batiments: List<Batiment>
+    private lateinit var batiments: MutableList<Batiment>
     private lateinit var filteredList: MutableList<Batiment>
     private lateinit var adapter: BatimentFragmentAdapter
 
     private lateinit var batimentService: BatimentService
+
+    //Pour la localisation
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,35 +46,73 @@ class AllBatimentFragment : Fragment(), SearchBarFragment.SearchListener {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_all_batiment, container, false)
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        getUserLocation()
+
         batimentService = BatimentService()
-
-        // Charger les bâtiments
-        batimentService.getBatiments().observe(viewLifecycleOwner, Observer { batiments ->
-            if (batiments != null) {
-                for (batiment in batiments) {
-                    //ajouterLieuSurCarte(batiment)
-                }
-            }
-        })
-
-        batiments = listOf(
-            Batiment("1", "Bâtiment enseignement A", "Batiment Enseignement", "", "", "", "Campus Nord", "500m", R.drawable.img),
-            Batiment("2", "Qwerty enseignement B", "Batiment Enseignement", "", "", "", "Campus Sud", "300m", R.drawable.img),
-            Batiment("3", "Asdfgh enseignement C", "Batiment Enseignement", "", "", "", "Campus Sud", "300m", R.drawable.img),
-            Batiment("4", "Zxcvb enseignement D", "Batiment Enseignement", "", "", "", "Campus Sud", "300m", R.drawable.img),
-            Batiment("5", "Bâtiment enseignement E", "Batiment Enseignement", "", "", "", "Campus Sud", "300m", R.drawable.img),
-            Batiment("6", "Bâtiment enseignement F", "Batiment Enseignement", "", "", "", "Campus Sud", "300m", R.drawable.img),
-            Batiment("7", "Bâtiment enseignement G", "Batiment Enseignement", "", "", "", "Campus Sud", "300m", R.drawable.img),
-        )
-        filteredList = batiments.toMutableList()
-
-        adapter = BatimentFragmentAdapter(batiments)
-        val recyclerAllBatiments = view.findViewById<RecyclerView>(R.id.recyclerAllBatiments)
-        recyclerAllBatiments.layoutManager =
-            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
-        recyclerAllBatiments.adapter = adapter
+        batiments = mutableListOf()
+        filteredList = mutableListOf()
 
         return view
+    }
+
+    private fun getUserLocation() {
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1001)
+            return
+        }
+
+        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+            location?.let {
+                val userGeoPoint = GeoPoint(it.latitude, it.longitude)
+                updateBatimentsDistances(userGeoPoint)
+            }
+        }
+    }
+
+    private fun updateBatimentsDistances(userLocation: GeoPoint) {
+        Log.d("AllBatimentFragement", "updateBatimentsDistances appelée avec : $userLocation")
+        // Charger les bâtiments
+        batimentService.getBatiments().observe(viewLifecycleOwner, Observer { bats ->
+            if (bats != null) {
+                for (batiment in bats) {
+                    try {
+                        val batimentLocation = GeoPoint(batiment.latitude.toDouble(), batiment.longitude.toDouble())
+                        val distance = calculateDistance(userLocation, batimentLocation)
+                        Log.d("AllBatimentFragement", "Distance calculée pour ${batiment.nom}: $distance mètres")
+
+                        // Mettre à jour l'objet Batiment avec la nouvelle distance
+                        batiment.distance = String.format("%.2f m", distance)
+
+                        Log.d("AllBatimentFragement", "Distance mise à jour pour ${batiment.nom}: ${batiment.distance}")
+
+                    } catch (e: Exception) {
+                        Log.e("AllBatimentFragement", "Erreur lors de la mise à jour de la distance pour ${batiment.nom}", e)
+                    }
+                    //ajouterLieuSurCarte(batiment)
+                    batiments.add(batiment)
+                }
+            }
+            filteredList = batiments.toMutableList()
+
+            adapter = BatimentFragmentAdapter(batiments)
+            val recyclerAllBatiments = view?.findViewById<RecyclerView>(R.id.recyclerAllBatiments)
+            recyclerAllBatiments?.layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+            recyclerAllBatiments?.adapter = adapter
+        })
+    }
+
+    private fun calculateDistance(start: GeoPoint, end: GeoPoint): Double {
+        val results = FloatArray(1)
+        android.location.Location.distanceBetween(
+            start.latitude, start.longitude,
+            end.latitude, end.longitude,
+            results
+        )
+        return results[0].toDouble() // Retourne la distance en mètres
     }
 
     override fun onResume() {
