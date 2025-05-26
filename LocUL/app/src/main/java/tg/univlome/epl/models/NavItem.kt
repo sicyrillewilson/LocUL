640bea04cfcdeace87592c486b06b6d39c74658a
sicyrillewilson <sicyrillewilson@gmail.com>
2025-05-26 11:23:59 +0000

    cyrille59_Documentation

--- LocUL/app/src/main/java/tg/univlome/epl/models/NavItem.kt
+++ LocUL/app/src/main/java/tg/univlome/epl/models/NavItem.kt
@@ -5,6 +5,41 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 
+/**
+ * Classe NavItem : Élément de navigation personnalisé dans l'interface utilisateur.
+ *
+ * Description :
+ * Cette classe encapsule les composants visuels et logiques d’un item de navigation
+ * dans l’application, permettant d’associer une vue de navigation (texte + icône + layouts)
+ * à un fragment correspondant.
+ *
+ * Utilisation :
+ * - Employé dans les fragments comme `BatimentFragment` ou `InfraFragment`
+ *   pour basculer dynamiquement entre différents sous-fragments.
+ * - Fournit une structure pratique pour gérer l’état de sélection et les interactions UI.
+ *
+ * Composants :
+ * @property layout Le layout principal de l’item (incluant l’icône et le texte)
+ * @property minLayout Layout réduit ou secondaire, souvent utilisé pour l’effet de sélection
+ * @property textView Texte affiché pour cet item (ex: "Tous", "Nord", "Sud")
+ * @property icon Icône associée à cet item
+ * @property fragment Fragment associé à cet item, à afficher lors de la sélection
+ *
+ * Exemple :
+ * ```kotlin
+ * val navItem = NavItem(
+ *     layout = view.findViewById(R.id.nav_all),
+ *     minLayout = view.findViewById(R.id.indicator_all),
+ *     textView = view.findViewById(R.id.txt_all),
+ *     icon = view.findViewById(R.id.icon_all),
+ *     fragment = AllBatimentFragment()
+ * )
+ * ```
+ *
+ * @see androidx.fragment.app.Fragment
+ * @see tg.univlome.epl.ui.batiment.BatimentFragment
+ * @see tg.univlome.epl.ui.infrastructure.InfraFragment
+ */
 data class NavItem(
     val layout: LinearLayout,
     val minLayout: LinearLayout,
--- LocUL/app/src/main/java/tg/univlome/epl/models/Salle.kt
+++ LocUL/app/src/main/java/tg/univlome/epl/models/Salle.kt
@@ -2,11 +2,56 @@ package tg.univlome.epl.models
 
 import java.io.Serializable
 
+/**
+ * Classe Salle : Représentation d'une salle physique sur le campus universitaire.
+ *
+ * Description :
+ * Cette classe hérite de [Lieu] et représente une salle d’enseignement, de réunion ou de laboratoire.
+ * Elle contient des informations spécifiques telles que sa capacité d’accueil et l’identifiant du bâtiment
+ * auquel elle appartient.
+ *
+ * Composants spécifiques :
+ * @property infrastructureId Identifiant du bâtiment auquel cette salle est rattachée (clé étrangère)
+ * @property capacite Capacité d’accueil de la salle (ex : "40", "100", "15")
+ *
+ * Hérite de :
+ * - [Lieu] : fournit les coordonnées, nom, description, image principale, type et autres attributs communs
+ *
+ * Constructeur secondaire :
+ * Permet d’instancier une salle avec toutes ses propriétés héritées et spécifiques
+ *
+ * Utilisation :
+ * - Affichée dans les listes ou détails via les adaptateurs comme [tg.univlome.epl.adapter.SalleAdapter]
+ * - Consultée dans [tg.univlome.epl.ui.home.SalleActivity] pour les détails
+ * - Chargée dynamiquement via [tg.univlome.epl.services.SalleService]
+ *
+ * @see Lieu
+ * @see Batiment
+ * @see Infrastructure
+ * @see tg.univlome.epl.services.SalleService
+ * @see tg.univlome.epl.ui.home.SalleActivity
+ */
 data class Salle(
     var infrastructureId: String = "",
     var capacite: String = ""
 ) : Lieu(), Serializable {
 
+    /**
+     * Constructeur secondaire pour instancier une salle avec toutes ses propriétés.
+     *
+     * @param id Identifiant unique de la salle
+     * @param infrastructureId ID du bâtiment parent
+     * @param nom Nom de la salle
+     * @param description Description textuelle
+     * @param capacite Capacité d'accueil
+     * @param longitude Coordonnée longitude
+     * @param latitude Coordonnée latitude
+     * @param image URL de l’image principale
+     * @param situation Zone ou localisation (Nord/Sud)
+     * @param type Type ou fonction de la salle
+     * @param images Liste d’images supplémentaires
+     * @param distance Distance par rapport à l'utilisateur
+     */
     constructor(
         id: String,
         infrastructureId: String,
